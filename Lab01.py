e5623d7cea5da79f7dfe849f597e296be5587f5c
jovicuna <jovicuna@udec.cl>
2016-03-31 11:44:15 -0300

    Primera ejecucion del codigo completo

--- Lab01.py
+++ Lab01.py
@@ -1,1 +1,11 @@
+#!/usr/bin/env python
+
 from SimpleCV import Camera, Display, Image
+cam = Camera()
+
+i = cam.getImage()
+i.save('Foto Kawaii.png')
+i.show()
+
+#CODIGO 100% REAL NO FAKE 1 PYTHON CRACK MEDICINA
+
