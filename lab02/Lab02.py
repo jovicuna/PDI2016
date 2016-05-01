f95e2f68faea5712536876245677f85ee0ba5267
jovicuna <jovicuna@udec.cl>
2016-05-01 17:38:39 -0300

    Se trabaja el ploteo y posterior segmentacion mediante binarizacion

--- lab02/Lab02.py
+++ lab02/Lab02.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python
 from SimpleCV import Camera, Image, Display
 import matplotlib.pyplot as plt
+import numpy as np
 c = Camera()
+#Obtencion de la imagen
 i = c.getImage()
 i.save('imagen_normal.png')
+#Escala de grises
 ig = i.grayscale()
 ig.save('imagen_grises.png')
 ig.show()
-hist = ig.histogram()
+#Histograma
+hist = ig.histogram(255)
 plt.plot(hist)
+max = np.max(hist)
+#Segmentacion
+ibin = ig.binarize(max)
+ibin.save('imagen_binaria.png')
+ibin.show()
+#Muestra del histograma
 plt.show()
